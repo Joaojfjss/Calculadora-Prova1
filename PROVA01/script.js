7755e472675a207ca407596bba27f9d73b4ab9f2
Joaojfjss <jfjss10@gmail.com>
2025-01-21 13:06:01 -0300

    Add files via upload

---
+++ PROVA01/script.js
@@ -0,0 +1,8 @@
+let numero = Number(prompt('Qual o primeiro número?'))// soma ou subtração de variaveis
+let numero1 = Number(prompt('Qual o segundo número'))
+
+console.log (numero += numero1)
+console.log (numero -= numero1)
+console.log (numero *= numero1)
+console.log (numero /= numero1)
+console.log (numero %= numero1)
